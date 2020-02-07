8855d3f14d6b520456681dc87ff82335a0fb9a9d
akshayrajp <akj.rajpurohit@gmail.com>
2020-02-08 03:44:34 +0530

    got that sweet db working :)

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,19 @@
-const getData = require("./app.js");
+const getData = require("./carddetails.json");
 
 const { Pool, Client } = require("pg");
 
 const pool = new Pool({
-  user: "sai",
-  host: "localhost",
+  user: "postgres",
+  host: "127.0.0.1",
   database: "student",
-  password: "qazwsx123",
-  port: "8000"
+  password: "1234",
+  port: "5432"
 });
 
-var cd = getData();
-
+var cd = getData;
 pool.query(
-  `INSERT INTO student(cname, cmname, cfname, cemp, caddres, clang, caadhar, cgender, cemail, cmobile, cseclang, cdob, cecp, cqd, cflang, ccat, cstatus, crelax)
-    VALUES(${cd.cname},${cd.cmname},${cd.cfname},${cd.cemp},${cd.caddres},${cd.clang},${cd.caadhar},${cd.cgender},${cd.cemail},${cd.cmobile},${cd.cseclang},${cd.cdob},${cd.cecp},${cd.cqd},${cd.cflang},${cd.ccat},${cd.cstatus},${cd.crelax});`,
+  `INSERT INTO candidate(cname, cmname, cfname, cemp, caddress, clang, caadhar, cgender, cemail, cmobile, cseclang, cdob, cecp, cqd, cflang, ccat, cstatus, crelax)
+    VALUES('${cd.ciname}','${cd.cmname}','${cd.cfname}','${cd.cemp}','${cd.caddress}','${cd.clang}',${cd.caadhar},'${cd.cgender}','${cd.cemail}','${cd.cmobile}','${cd.cseclang}','${cd.cdob}','${cd.cecp}','${cd.cqd}','${cd.cflang}','${cd.ccat}','${cd.cstatus}','${cd.crelax}');`,
   (err, res) => {
     console.log(err, res);
     pool.end();
